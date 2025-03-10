0d726779ef22527295ed5bee16e696d20f83f9a4
Goh Zi He <zihe.goh@gmail.com>
2025-03-10 22:29:53 +0800

    feat: some basic checks

--- Program.fs
+++ Program.fs
@@ -15,6 +15,8 @@ let WHITE_KING = '♔'
 
 let initBoard =
     let row = List.init 8 (fun _ -> '.')
+    let black_pawns = List.init 8 (fun _ -> BLACK_PAWN)
+    let white_pawns = List.init 8 (fun _ -> WHITE_PAWN)
 
     let board =
         [ [ BLACK_ROOK
@@ -25,26 +27,12 @@ let initBoard =
             BLACK_BISHOP
             BLACK_KNIGHT
             BLACK_ROOK ]
-          [ BLACK_PAWN
-            BLACK_PAWN
-            BLACK_PAWN
-            BLACK_PAWN
-            BLACK_PAWN
-            BLACK_PAWN
-            BLACK_PAWN
-            BLACK_PAWN ]
+          black_pawns
           row
           row
           row
           row
-          [ WHITE_PAWN
-            WHITE_PAWN
-            WHITE_PAWN
-            WHITE_PAWN
-            WHITE_PAWN
-            WHITE_PAWN
-            WHITE_PAWN
-            WHITE_PAWN ]
+          white_pawns
           [ WHITE_ROOK
             WHITE_KNIGHT
             WHITE_BISHOP
@@ -71,23 +59,55 @@ let printBoard board =
     printfn "  ─────────────────"
     printfn "   a b c d e f g h"
 
-let validateMoves input = false
+let validateMove (inputs: list<string>) =
+    let inputs =
+        inputs
+        |> List.filter (fun x -> x.Length = 2 && Char.IsAsciiLetterLower x[0] && Char.IsAsciiDigit x[1])
+
+    match inputs.Length with
+    | 2 -> true
+    | _ -> false
+
+let getMoves (input: string) =
+    // Make sure input is valid
+    let parts = input.Split ' '
+
+    match parts.Length with
+    | 1 ->
+        match parts.[0] with
+        | "0-0"
+        | "0-0-0" -> Some input
+        | _ -> None
+    | _ ->
+        match validateMove [ parts.[0]; parts.[1] ] with
+        | true -> Some input
+        | false -> None
 
 // Main game loop
 let rec gameLoop board =
     printBoard board
     printf "\nEnter move (e.g. 'e2 e4') or 'quit' to exit: "
+
     let input = Console.ReadLine()
 
     match input.ToLower() with
-    | "quit" -> printfn "Thanks for playing!"
+    | "quit"
+    | "q" -> printfn "Thanks for playing!"
     | _ ->
-        // TODO: implement move validation and execution
-        match validateMoves input with
-        | true -> printfn "Valid moves"
-        | false -> printfn "Invalid moves, please try again."
+        match String.IsNullOrWhiteSpace input with
+        | true ->
+            printfn "Invalid input, type in something."
+            Console.ReadLine() |> ignore
+        | false ->
+            match getMoves input with
+            | None ->
+                printf "Invalid moves, please try again. Press enter to continue."
+                Console.ReadLine() |> ignore
+            | Some _ ->
+                printfn "%s is a valid moves" input
+                Console.ReadLine() |> ignore
 
-        gameLoop board
+            gameLoop board
 
 // Start the game
 let board = initBoard
