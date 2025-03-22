5283f44a706060e8e8463652973705ee33a60885
Goh Zi He <zihe.goh@gmail.com>
2025-03-22 15:06:00 +0800

    test: queen and king moves check

--- Chess.Tests/Tests.fs
+++ Chess.Tests/Tests.fs
@@ -3,94 +3,20 @@
 open Chess
 open Xunit
 
-// TODO: Start adding more tests on checking valid moves
-
+let hasNoDuplicates list =
+    let distinctList = List.distinct list
+    list.Length = distinctList.Length
 
 let assertValid (expected: list<string>) (res: list<Location>) =
     Assert.Equal(expected.Length, res.Length)
+    Assert.True(hasNoDuplicates expected)
+    Assert.True(hasNoDuplicates res)
 
     let isValid (res: list<Location>) =
         let pred (loc: string) = Assert.Contains(getSquare loc, res)
         pred
 
     expected |> List.iter (isValid res)
-
-
-[<Fact>]
-let ``Check knight moves`` () =
-    // Top left corner
-    let expected = [ "b6"; "c7" ]
-    let res = generateKnightMove (getSquare "a8")
-    assertValid expected res
-
-    // Top right corner
-    let expected = [ "g6"; "f7" ]
-    let res = generateKnightMove (getSquare "h8")
-    assertValid expected res
-
-    // Bottom left corner
-    let expected = [ "b3"; "c2" ]
-    let res = generateKnightMove (getSquare "a1")
-    assertValid expected res
-
-    // Bottom right corner
-    let expected = [ "g3"; "f2" ]
-    let res = generateKnightMove (getSquare "h1")
-    assertValid expected res
-
-    // At the side of the board
-    let expected = [ "b7"; "c6"; "c4"; "b3" ]
-    let res = generateKnightMove (getSquare "a5")
-    assertValid expected res
-
-    // In the middle of the board
-    let expected = [ "b8"; "d8"; "a7"; "e7"; "a5"; "e5"; "b4"; "d4" ]
-    let res = generateKnightMove (getSquare "c6")
-    assertValid expected res
-
-
-[<Fact>]
-let ``Check rook move`` () =
-    // Top left corner
-    let expected =
-        [ "a7"
-          "a6"
-          "a5"
-          "a4"
-          "a3"
-          "a2"
-          "a1"
-          "b8"
-          "c8"
-          "d8"
-          "e8"
-          "f8"
-          "g8"
-          "h8" ]
-
-    let res = generateRookMove (getSquare "a8")
-    assertValid expected res
-
-    // Center
-    let expected =
-        [ "d1"
-          "d2"
-          "d3"
-          "d4"
-          "d6"
-          "d7"
-          "d8"
-          "a5"
-          "b5"
-          "c5"
-          "e5"
-          "f5"
-          "g5"
-          "h5" ]
-
-    let res = generateRookMove (getSquare "d5")
-    assertValid expected res
-
 
 [<Fact>]
 let ``Check pawn move`` () =
@@ -124,6 +50,140 @@ let ``Check pawn move`` () =
     assertValid expected res
 
 [<Fact>]
+let ``Check knight moves`` () =
+    // Top left corner
+    let expected = [ "b6"; "c7" ]
+    let res = generateKnightMove (getSquare "a8")
+    assertValid expected res
+
+    // Top right corner
+    let expected = [ "g6"; "f7" ]
+    let res = generateKnightMove (getSquare "h8")
+    assertValid expected res
+
+    // Bottom left corner
+    let expected = [ "b3"; "c2" ]
+    let res = generateKnightMove (getSquare "a1")
+    assertValid expected res
+
+    // Bottom right corner
+    let expected = [ "g3"; "f2" ]
+    let res = generateKnightMove (getSquare "h1")
+    assertValid expected res
+
+    // At the side of the board
+    let expected = [ "b7"; "c6"; "c4"; "b3" ]
+    let res = generateKnightMove (getSquare "a5")
+    assertValid expected res
+
+    // In the middle of the board
+    let expected = [ "b8"; "d8"; "a7"; "e7"; "a5"; "e5"; "b4"; "d4" ]
+    let res = generateKnightMove (getSquare "c6")
+    assertValid expected res
+
+[<Fact>]
+let ``Check rook move`` () =
+    // Top left corner
+    let expected =
+        [ "a7"
+          "a6"
+          "a5"
+          "a4"
+          "a3"
+          "a2"
+          "a1"
+          "b8"
+          "c8"
+          "d8"
+          "e8"
+          "f8"
+          "g8"
+          "h8" ]
+
+    let res = generateRookMove (getSquare "a8")
+    assertValid expected res
+
+    // Top right corner
+    let expected =
+        [ "h7"
+          "h6"
+          "h5"
+          "h4"
+          "h3"
+          "h2"
+          "h1"
+          "a8"
+          "b8"
+          "c8"
+          "d8"
+          "e8"
+          "f8"
+          "g8" ]
+
+    let res = generateRookMove (getSquare "h8")
+    assertValid expected res
+
+    // Bottom left corner
+    let expected =
+        [ "a8"
+          "a7"
+          "a6"
+          "a5"
+          "a4"
+          "a3"
+          "a2"
+          "b1"
+          "c1"
+          "d1"
+          "e1"
+          "f1"
+          "g1"
+          "h1" ]
+
+    let res = generateRookMove (getSquare "a1")
+    assertValid expected res
+
+    // Bottom right corner
+    let expected =
+        [ "h8"
+          "h7"
+          "h6"
+          "h5"
+          "h4"
+          "h3"
+          "h2"
+          "a1"
+          "b1"
+          "c1"
+          "d1"
+          "e1"
+          "f1"
+          "g1" ]
+
+    let res = generateRookMove (getSquare "h1")
+    assertValid expected res
+
+    // Center
+    let expected =
+        [ "d1"
+          "d2"
+          "d3"
+          "d4"
+          "d6"
+          "d7"
+          "d8"
+          "a5"
+          "b5"
+          "c5"
+          "e5"
+          "f5"
+          "g5"
+          "h5" ]
+
+    let res = generateRookMove (getSquare "d5")
+    assertValid expected res
+
+[<Fact>]
 let ``Check bishop move`` () =
     // Top left
     let expected = [ "b7"; "c6"; "d5"; "e4"; "f3"; "g2"; "h1" ]
@@ -143,4 +203,141 @@ let ``Check bishop move`` () =
     // Bottom right
     let expected = [ "a8"; "b7"; "c6"; "d5"; "e4"; "f3"; "g2" ]
     let res = generateBishopMove (getSquare "h1")
+    assertValid expected res
+
+[<Fact>]
+let ``Check queen move`` () =
+    // Top left
+    let expected =
+        [ "b7"
+          "c6"
+          "d5"
+          "e4"
+          "f3"
+          "g2"
+          "h1"
+          "a7"
+          "a6"
+          "a5"
+          "a4"
+          "a3"
+          "a2"
+          "a1"
+          "b8"
+          "c8"
+          "d8"
+          "e8"
+          "f8"
+          "g8"
+          "h8" ]
+
+    let res = generateQueenMove (getSquare "a8")
+    assertValid expected res
+
+    // Top right
+    let expected =
+        [ "h7"
+          "h6"
+          "h5"
+          "h4"
+          "h3"
+          "h2"
+          "h1"
+          "a8"
+          "b8"
+          "c8"
+          "d8"
+          "e8"
+          "f8"
+          "g8"
+          "g7"
+          "f6"
+          "e5"
+          "d4"
+          "c3"
+          "b2"
+          "a1" ]
+
+    let res = generateQueenMove (getSquare "h8")
+    assertValid expected res
+
+    // Bottom left corner
+    let expected =
+        [ "a8"
+          "a7"
+          "a6"
+          "a5"
+          "a4"
+          "a3"
+          "a2"
+          "b1"
+          "c1"
+          "d1"
+          "e1"
+          "f1"
+          "g1"
+          "h1"
+          "b2"
+          "c3"
+          "d4"
+          "e5"
+          "f6"
+          "g7"
+          "h8" ]
+
+    let res = generateQueenMove (getSquare "a1")
+    assertValid expected res
+
+    // Bottom right corner
+    let expected =
+        [ "h8"
+          "h7"
+          "h6"
+          "h5"
+          "h4"
+          "h3"
+          "h2"
+          "a1"
+          "b1"
+          "c1"
+          "d1"
+          "e1"
+          "f1"
+          "g1"
+          "a8"
+          "b7"
+          "c6"
+          "d5"
+          "e4"
+          "f3"
+          "g2" ]
+
+    let res = generateQueenMove (getSquare "h1")
+    assertValid expected res
+
+[<Fact>]
+let ``Check king move`` () =
+    // Top left
+    let expected = [ "a7"; "b7"; "b8" ]
+    let res = generateKingMove (getSquare "a8")
+    assertValid expected res
+
+    // Top right
+    let expected = [ "h7"; "g7"; "g8" ]
+    let res = generateKingMove (getSquare "h8")
+    assertValid expected res
+
+    // Bottom left
+    let expected = [ "a2"; "b2"; "b1" ]
+    let res = generateKingMove (getSquare "a1")
+    assertValid expected res
+
+    // Bottom right
+    let expected = [ "g1"; "g2"; "h2" ]
+    let res = generateKingMove (getSquare "h1")
+    assertValid expected res
+
+    // Center
+    let expected = [ "c5"; "c6"; "d6"; "e6"; "e5"; "e4"; "d4"; "c4" ]
+    let res = generateKingMove (getSquare "d5")
     assertValid expected res
